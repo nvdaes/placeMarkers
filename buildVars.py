6e8267d65d5257ef9d62acb90614ff307c3bf3c3
Noelia Ruiz Martínez <nrm1977@gmail.com>
2013-05-29 19:03:08 +0200

    Included installTasks.py as file with translatable messages.

--- buildVars.py
+++ buildVars.py
@@ -16,7 +16,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on installation and add-on information
 	"addon-description" : _("Add-on for setting place markers on specific virtual documents"),
 	# version
-	"addon-version" : "2.0-dev",
+	"addon-version" : "1.0-dev",
 	# Author(s)
 	"addon-author" : "Noelia <nrm1977@gmail.com>, Chris <llajta2012@hotmail.com>",
 	# URL for the add-on documentation support
@@ -28,7 +28,7 @@ import os.path
 
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
-pythonSources = [os.path.join("addon","globalPlugins","*.py")]
+pythonSources = [os.path.join("addon", "*.py"), os.path.join("addon", "globalPlugins", "*.py")]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
