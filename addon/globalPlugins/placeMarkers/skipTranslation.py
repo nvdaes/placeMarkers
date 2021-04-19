1c3017b73075f598e9d9fcb3ac57bbb061f819eb
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 <grisov.github@mailnull.com>
2021-04-19 11:10:05 +0300

    Another linter error

--- addon/globalPlugins/placeMarkers/skipTranslation.py
+++ addon/globalPlugins/placeMarkers/skipTranslation.py
@@ -1,9 +1,9 @@
-# -*- coding: UTF-8 -*-
+﻿# -*- coding: UTF-8 -*-
 # skipTranslation: Module to get translated texts from NVDA
 # Based on implementation made by Alberto Buffolino
 # https://github.com/nvaccess/nvda/issues/4652
 #Copyright (C) 2016 Noelia Ruiz Martínez
 # Released under GPL 2
 
-def translate(text):
+def translate(text:str):
 	return _(text)
